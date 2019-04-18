524ff6a4b9c04b1985b16a7e57569f27397fdb63
Daniel K Yamamoto <48425390+danielkeizoy@users.noreply.github.com>
2019-04-18 01:25:39 -0300

    Ver descricao CursoSuperior
    Mesma estrutura de CursoSuperior.java
    apenas a diferença é nas médias que em Superior é 7,0 para passar direto, e pós é 5,0
    e no getMedia( ), em que:
    no Sup é diretamente a média entre a média principal e exame,
    no Pós é diretamente o valor 5,0, caso a média principal seja maior ou igual a 5,0 , e caso seja menor, vira a média entre principal e exame
    Comparativamente no caso:
    Sup:
    
    if (MÉDIA principal >=7){ 
    PASSOU 
    }
    else {
    média final do curso =  (Média principal + exame) / 2
    }
    -----------------
    Pós:
    
    if (MÉDIA principal >= 5){ 
    PASSOU
    }
    else{ 
           if ((MÉDIA principal +exame)/2 >=5){
           média final do curso = 5,0
           }
           else {
           média final do curso = (MÉDIA principal +exame)/2
           }
    }
    
    caraio que descricao longa, toma uma batata ae

--- src/model/curso/CursoDePos.java
+++ src/model/curso/CursoDePos.java
@@ -1,7 +1,111 @@
-
 package model.curso;
-
 
 public class CursoDePos extends Curso {
     
+    private double notanp1; 
+    private double notanp2;
+    private double reposicao;
+    private double exame;
+    private double media;
+    private double mediaFinal;
+
+    public CursoDePos (String aNome) {
+        super(aNome);
+                
+        this.notanp1 = 0.0;
+        this.notanp2 = 0.0;
+        this.reposicao = 0.0;
+        this.exame = 0.0;
+        this.media = 0.0;
+        this.mediaFinal = 0.0;
+        
+    }
+    
+    public void setNotaP1(double nota) {
+        this.notanp1 = nota; 
+    }
+
+    public void setNotaP2(double nota){
+        this.notanp2 = nota; 
+    }
+
+    public void setNotaReposicao(double nota) {
+        this.reposicao = nota; 
+    }
+
+    public void setNotaExame(double nota) {
+        this.exame = nota; 
+    }
+  
+    public double getNotaP1(){
+        return notanp1;
+    }
+
+    public double getNotaP2(){
+        return notanp2;
+    }
+
+    public double getNotaReposicao() {
+        return reposicao;
+    }
+
+    public double getNotaExame(){
+        return exame;
+    }
+    
+   
+    @Override
+    public double getMedia(){
+        // TESTE DE NOTA 
+        
+        if ((this.reposicao > this.notanp1) || (this.reposicao > this.notanp2)) {
+            if (this.reposicao > this.notanp1) {
+            	if( ( (this.reposicao + this.notanp2) /2) < 5) {
+            		this.mediaFinal = (( (this.reposicao + this.notanp2) /2) + exame)/2;
+            		if(this.mediaFinal >= 5) {
+            			this.mediaFinal = 5;
+            		}
+            	};       
+            }
+            else{
+            	if( ( (this.reposicao + this.notanp1) /2) < 5) {
+            		this.mediaFinal = (( (this.reposicao + this.notanp1) /2) + exame)/2;
+            		if(this.mediaFinal >= 5) {
+            			this.mediaFinal = 5;
+            		}
+            	};
+            }
+        }
+        return mediaFinal;
+    }
+    
+    @Override
+    public boolean isPassou(){
+    	this.media = (this.notanp1 + this.notanp2) / 2;
+        if (media >= 5){
+            return true;
+        } 
+        else{            
+            return false;
+        }
+    }
+    
+    @Override
+    public boolean isAprovado(){
+        boolean status = isPassou();
+        if(status == true) {
+        	System.out.println("Status: Aprovado!");
+            return true;
+        }
+        else {
+        	if (mediaFinal>= 5){
+        		System.out.println("Status: Aprovado!");
+        		return true;
+        	} 
+        	else{
+        		System.out.println("Status: Reprovado.");
+        		return false;
+        	}
+        }
+    }
 }
